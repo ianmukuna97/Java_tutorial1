85be19337bc79ed8b6de461170121320b51371da
ianmukuna97 <ianmukuna97@gmail.com>
2025-08-11 15:32:22 +0300

    Java Tutorial 4 and 5

--- src/main/java/org/example/tutorial_4.java
+++ src/main/java/org/example/tutorial_4.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class tutorial_4 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int length = 5;
         int width = 4;
         int area;
@@ -13,5 +13,19 @@ public class tutorial_4 {
         System.out.println("Length is: " + length);
         System.out.println("Width is: " + width);
         System.out.println("Area of a rectangle is: " + area);
+
+        byte myNum = 127;
+        System.out.println(myNum);
+
+        // Create variables of different data types
+        int items = 50;
+        float costPerItem = 9.99f;
+        float totalCost = items * costPerItem;
+        char currency = '$';
+
+        // Print variables
+        System.out.println("Number of items: " + items);
+        System.out.println("Cost per item: " + costPerItem + currency);
+        System.out.println("Total cost = " + totalCost + currency);
     }
 }
---
+++ src/main/java/org/example/tutorial_5.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class tutorial_5 {
+    public static void main(String[] args) {
+        int myInt = 9;
+        double myDouble = myInt; // Automatic casting: int to double
+
+        System.out.println(myInt);      // Outputs 9
+        System.out.println(myDouble);   // Outputs 9.0
+    }
+}
+
+
