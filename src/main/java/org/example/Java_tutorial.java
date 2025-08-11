82654f9eefecf16b68ad1d5b749358f2be12c0bd
ianmukuna97 <ianmukuna97@gmail.com>
2025-08-11 14:12:05 +0300

    Java Tutorial

---
+++ src/main/java/org/example/Java_tutorial.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Java_tutorial {
+    public static void main(String[] args) {
+        System.out.println("Hello World"); //Outputs Hello World
+        System.out.println("I am learning Java.");
+        System.out.println(500);
+        System.out.println(5 + 5);
+        //Multiplication of 4*5
+        System.out.println(4 * 5);
+        String name = "John";
+        System.out.println(name);
+
+        int myNum = 20;
+        myNum = 15;
+        System.out.println(myNum);
+
+        float myfloatNum = 5.0f;
+        myfloatNum = 4.5f;
+        System.out.println(myfloatNum);
+    }
+}
