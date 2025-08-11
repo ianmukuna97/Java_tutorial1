2a5c8f0ae48a7b47271d885f98af83f0e214fec0
ianmukuna97 <ianmukuna97@gmail.com>
2025-08-11 14:51:20 +0300

    Java Tutorial 4

---
+++ src/main/java/org/example/tutorial_4.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class tutorial_4 {
+    public static void main(String[] args){
+        int length = 5;
+        int width = 4;
+        int area;
+
+        //Area of a rectangle
+        area = length * width;
+
+        //Print variables
+        System.out.println("Length is: " + length);
+        System.out.println("Width is: " + width);
+        System.out.println("Area of a rectangle is: " + area);
+    }
+}
