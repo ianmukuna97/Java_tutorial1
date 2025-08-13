c2c623e34e3d9b21962aad2336a8f17459c459ef
ianmukuna97 <ianmukuna97@gmail.com>
2025-08-13 17:00:59 +0300

    Java Tutorial 6 & 7

---
+++ src/main/java/org/example/tutorial_6.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class tutorial_6 {
+    public static void main(String[] args){
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+
+        String greeting = "Hello World";
+        System.out.println(greeting);
+
+        int sum1 = 100+50;
+        System.out.println(sum1);
+
+        String txt = "Hello World";
+        System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
+        System.out.println(txt.toLowerCase());   // Outputs "hello world"
+
+
+    }
+}
---
+++ src/main/java/org/example/tutorial_7.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class tutorial_7 {
+    public static void main(String[] args){
+        String txt = "Please locate where 'locate' occurs!";
+        System.out.println(txt.indexOf("locate")); // Outputs 7
+
+    }
+}
